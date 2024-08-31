f8c883d987c544b28e4829558045572d716dc3ca
SayanDasguptaSDG <sayan.dg1995@gmail.com>
2024-08-31 22:26:04 +0530

    introducing BaseTest for initial configuration and teardown

--- src/test/java/com/appium/practice/AppiumBasics.java
+++ src/test/java/com/appium/practice/AppiumBasics.java
@@ -1,37 +1,10 @@
 package com.appium.practice;
 import io.appium.java_client.AppiumBy;
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.options.UiAutomator2Options;
-import io.appium.java_client.service.local.AppiumDriverLocalService;
-import io.appium.java_client.service.local.AppiumServiceBuilder;
 import org.testng.annotations.Test;
 
-import java.io.File;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
-
-public class AppiumBasics {
+public class AppiumBasics extends BaseTest {
     @Test
-    public void AppiumTest() throws MalformedURLException, URISyntaxException {
-        AppiumDriverLocalService service = new AppiumServiceBuilder().
-                withAppiumJS(new File("C:\\Users\\sayan\\AppData\\Roaming" +
-                        "\\npm\\node_modules\\appium\\build\\lib\\main.js")).
-                withIPAddress("127.0.0.1").
-                usingPort(4723).
-                build();
-
-        service.start();
-        UiAutomator2Options options = new UiAutomator2Options();
-        options.setDeviceName("TestPhone");
-        options.setApp("D:\\APKFiles\\resources\\ApiDemos-debug.apk");
-
-        AndroidDriver driver = new AndroidDriver(
-                new URI("http://127.0.0.1:4723").toURL(), options);
-
+    public void WiFiSettingsName() {
         driver.findElement(AppiumBy.accessibilityId("Preference")).click();
-        driver.quit();
-        service.stop();
     }
-
 }
---
+++ src/test/java/com/appium/practice/BaseTest.java
@@ -0,0 +1,43 @@
+package com.appium.practice;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class BaseTest {
+    public AndroidDriver driver;
+    public AppiumDriverLocalService service;
+    public UiAutomator2Options options;
+
+    @BeforeClass
+    public void ConfigureAppium() throws URISyntaxException, MalformedURLException {
+        service = new AppiumServiceBuilder().
+                withAppiumJS(new File("C:\\Users\\sayan\\AppData\\Roaming" +
+                        "\\npm\\node_modules\\appium\\build\\lib\\main.js")).
+                withIPAddress("127.0.0.1").
+                usingPort(4723).
+                build();
+
+        service.start();
+        options = new UiAutomator2Options();
+        options.setDeviceName("TestPhone");
+        options.setApp("D:\\APKFiles\\resources\\ApiDemos-debug.apk");
+
+        driver = new AndroidDriver(
+                new URI("http://127.0.0.1:4723").toURL(), options);
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+        service.stop();
+    }
+}
