e423191f9a722f778a6cca8ab4bd4a08dd6e0b21
SayanDasguptaSDG <sayan.dg1995@gmail.com>
2024-08-31 23:20:26 +0530

    long press gesture

--- src/test/java/com/appium/practice/AppiumBasics.java
+++ src/test/java/com/appium/practice/AppiumBasics.java
@@ -1,5 +1,9 @@
 package com.appium.practice;
+import com.google.common.collect.ImmutableMap;
 import io.appium.java_client.AppiumBy;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -14,5 +18,17 @@ public class AppiumBasics extends BaseTest {
         Assert.assertEquals(alert_title, "WiFi settings");
         driver.findElement(AppiumBy.id("android:id/edit")).sendKeys("TestWiFi");
         driver.findElements(AppiumBy.className("android.widget.Button")).get(1).click();
+    }
+
+    @Test
+    public void LongPressGesture() throws InterruptedException {
+        driver.findElement(AppiumBy.accessibilityId("Views")).click();
+        driver.findElement(AppiumBy.accessibilityId("Expandable Lists")).click();
+        driver.findElement(AppiumBy.xpath("//android.widget.TextView[@content-desc=\"1. Custom Adapter\"]")).click();
+        WebElement element = driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text=\"People Names\"]"));
+        ((JavascriptExecutor) driver).executeScript("mobile:longClickGesture",
+                ImmutableMap.of("elementId", ((RemoteWebElement) element).getId(),
+                        "duration", 2000));
+        Thread.sleep(2000);
     }
 }
